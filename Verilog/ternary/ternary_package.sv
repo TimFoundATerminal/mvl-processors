5f52adb6189b52ffeb80f3cfe52507daef35f24d
Tim Tommins <tom.timmons@outlook.com>
2025-03-19 11:00:52 +0000

    remove old ternary attempts

--- Verilog/ternary/ternary_package.sv
+++
@@ -1,9 +0,0 @@
-package ternary_package;
-    parameter VPOS = 5.0;
-    parameter VZERO = 0.0;
-    parameter VNEG = -5.0;
-    parameter VTHRESH_POS = 2.5;
-    parameter VTHRESH_NEG = -2.5;
-    parameter VDD = 5.0;
-    parameter VSS = -5.0;
-endpackage
